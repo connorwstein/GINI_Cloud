4ca5654c8d10e134c700b58445d196b66d1b895f
connorwstein <connor.stein@mail.mcgill.ca>
2015-10-12 16:53:31 -0400

    more comments outlining the functions

--- AmazonCloudServer.py
+++ AmazonCloudServer.py
@@ -1,5 +1,6 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+from subprocess import call
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -24,13 +25,16 @@ server.register_introspection_functions()
 # class MyFuncs:
 #     def mul(self, x, y):
 #         return x * y
+
+#Server dependencies awscli, boto3, python3
 class AmazonCloudFunctions:
 	def testMultiply(self,x,y):
 		return x*y	#hello world function 
-	def configureAWS():
+	def configureAWS(key, secret_key):
 		#TODO: Sets up the users AWS creditentials and region
 		#so boto3 can be used
 		#then call self.ec2=boto3.resource('ec2')
+		#could just make a ~/.aws directory with the credentials
 	def listInstances():
 		#TODO: List all the instances available
 	def createInstance():
