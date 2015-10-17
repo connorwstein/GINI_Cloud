97f46903741a172ff1413bfbfac759c653460e1b
connorwstein <connor.stein@mail.mcgill.ca>
2015-10-17 13:33:38 -0400

    cleaned up code

--- AmazonCloudServer.py
+++ AmazonCloudServer.py
@@ -1,6 +1,5 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
-from subprocess import call
 import os
 import shutil
 import boto3
@@ -13,26 +12,7 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 server = SimpleXMLRPCServer(("localhost", 8000),requestHandler=RequestHandler, allow_none=True)
 server.register_introspection_functions()
 
-# Register pow() function; this will use the value of
-# pow.__name__ as the name, which is just 'pow'.
-# registering a built in function
-# server.register_function(pow) 
-
-# # Register a function under a different name
-# def adder_function(x,y):
-#     return x + y
-# server.register_function(adder_function, 'add')
-
-# # Register an instance; all the methods of the instance are
-# # published as XML-RPC methods (in this case, just 'mul').
-# class MyFuncs:
-#     def mul(self, x, y):
-#         return x * y
-
 #Server dependencies awscli, boto3, python3
-
-
-# When we init need to update the iptables on the machine
 class AmazonCloudFunctions:
 	def configure_aws(self,key,secret_key):
 		#Create the .aws directory with the users keys (same functionality as aws configure)
@@ -41,7 +21,7 @@ class AmazonCloudFunctions:
 			print("Directory exists, removing")
 			shutil.rmtree(path)
 		os.makedirs(path)
-		creds=open(os.path.join(path,"credentials"),'w')
+		creds=open(os.pat.join(path,"credentials"),'w')
 		config=open(os.path.join(path,"config"),'w')
 		config.write('[default]\nregion = us-west2'); # default region
 		creds.write('[default]\naws_access_key_id = '+key+'\naws_secret_access_key = '+secret_key);
@@ -52,10 +32,6 @@ class AmazonCloudFunctions:
 		print("list")
 	def create_instance():
 		print("create")
-
-
-
-
 
 server.register_instance(AmazonCloudFunctions())
 
