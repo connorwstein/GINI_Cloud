65d3451add1cc7053f8f65ee6048f0274532a7dc
connorwstein <connor.stein@mail.mcgill.ca>
2015-10-26 17:58:40 -0400

    can create a working pem key in the server

--- AmazonCloudServer.py
+++ AmazonCloudServer.py
@@ -3,7 +3,7 @@ from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 import os
 import shutil
 import boto3
-
+import time
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
 	rpc_paths=('/RPC2',)
@@ -16,9 +16,9 @@ server.register_introspection_functions()
 class AmazonCloudFunctions:
 	
 	def __init__(self):
-		self.instances = []
 		self.key_pair = None
 		self.key_name = "GINI"
+		self.ec2 = None
 
 	def configure_aws(self,key,secret_key):
 		#Create the .aws directory with the users keys (same functionality as aws configure)
@@ -29,30 +29,45 @@ class AmazonCloudFunctions:
 		os.makedirs(path)
 		creds=open(os.path.join(path,"credentials"),'w')
 		config=open(os.path.join(path,"config"),'w')
-		config.write('[default]\nregion = us-west-2'); # default region
-		creds.write('[default]\naws_access_key_id = '+key+'\naws_secret_access_key = '+secret_key);
+		config.write('[default]\nregion = us-west-2\n'); # default region
+		creds.write('[default]\naws_access_key_id = '+key+'\naws_secret_access_key = '+secret_key+'\n');
 		creds.close()
 		config.close()
-		ec2=boto3.resource('ec2') # ec2 now available for the other methods
-		for inst in ec2.instances.all():
-			self.instances.append(inst)
+		self.ec2=boto3.resource('ec2') # ec2 now available for the other methods
 	def list_instances(self):
-		for inst in self.instances:
+		for inst in self.ec2.instances.all():
 			print("ID: "+inst.id+" TYPE: "+inst.instance_type+" STATE: "+inst.state['Name']+" Public DNS: "+inst.public_dns_name)
 	def create_instance(self):
-		# TODO Bernie create an image and then create a instance
+		# If the GINI key does not exist, create it
+		found_key = 0
+		for key_info in self.ec2.key_pairs.all():
+			if key_info.key_name == self.key_name:
+				found_key = 1
+				break
+		if not found_key:
+			self.key_gen()
+		# This image ID is the default image file
+		new_instance = self.ec2.create_instances(ImageId="ami-0211f431", MinCount=1, MaxCount=1, InstanceType='t2.micro', KeyName = self.key_name)	
+		# Chill for that instance to be crafted
+		while(new_instance[0].state['Name'] != "running"):
+			new_instance[0].load()  # Get current status
+			print("ID: "+new_instance[0].id+" State: "+new_instance[0].state['Name'])
+			time.sleep(5) 
 	def stop_all_instances(self):
-		for inst in self.instances:
+		for inst in self.ec2.instances.all():
 			inst.stop()
-	def start_all_instances(self):
-		for inst in self.instances:
+	def terminate_all_instances(self):
+		for inst in self.ec2.instances.all():
 			inst.terminate()
 	def key_gen(self):
-    	self.key_pair=ec2.create_key_pair(False, self.key_name)
-   		# f = open(key_name+".pem",'w')
-     #    f.write(key_pair.key_material)
-     #    f.close()
-     #    os.chmod(f.name,0400)
+		# False means make the key for real
+		self.key_pair = self.ec2.create_key_pair(DryRun = False, KeyName = self.key_name)
+		if os.path.exists(self.key_name+".pem"):
+			os.chmod(self.key_name+".pem",0777)
+		f = open(self.key_name+".pem",'w')
+		f.write(self.key_pair.key_material)
+		f.close()
+		os.chmod(f.name,0400)
 
 server.register_instance(AmazonCloudFunctions())
 
--- TestClient.py
+++ TestClient.py
@@ -1,9 +1,11 @@
 import xmlrpclib
 import boto3
-
+import time
 s = xmlrpclib.ServerProxy('http://localhost:8000')
 #note add your keys before running the test client
-s.configure_aws("AKIAIM65WKOBI3B3ETKA","emIme22KJKEPwrNIVbZ4h+FLeUhDrwNgqKWt55su");
-s.list_instances();
+s.configure_aws("AKIAIM65WKOBI3B3ETKA","emIme22KJKEPwrNIVbZ4h+FLeUhDrwNgqKWt55su")
+s.list_instances()
+s.create_instance()
+s.list_instances()
 # Print list of available methods
 print(s.system.listMethods())
