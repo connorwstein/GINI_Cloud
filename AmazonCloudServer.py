adb0e66124ec32cd48df78bcf761406ecc104ce6
bernardkaminski <bernard.kaminski@mail.mcgill.ca>
2015-11-25 17:06:49 -0500

    whole tunnel creation is automated, made the testclient more user friendly, can reuse a created instance with get_running command

--- AmazonCloudServer.py
+++ AmazonCloudServer.py
@@ -5,6 +5,8 @@ import os
 import shutil
 import boto3
 import time
+import sys
+import multiprocessing
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
 	rpc_paths=('/RPC2',)
@@ -36,6 +38,9 @@ class AmazonCloudFunctions:
 		creds.close()
 		config.close()
 		self.ec2=boto3.resource('ec2') # ec2 now available for the other methods
+		if self.ec2.instances == None:
+			print "Unable to authenticate"
+			sys.exit(1)
 	def get_ip(self):
 		return self.new_instance_ip
 	def list_instances(self):
@@ -70,15 +75,26 @@ class AmazonCloudFunctions:
 		for inst in self.ec2.instances.all():
 			if inst.public_ip_address == ip:
 				inst.terminate()
+	def get_running_instance(self):
+		for inst in self.ec2.instances.all():
+			if inst != None:
+				self.new_instance_ip = inst.public_ip_address
+				break;
 	def key_gen(self):
 		# False means make the key for real
 		self.key_pair = self.ec2.create_key_pair(DryRun = False, KeyName = self.key_name)
-		if os.path.exists(self.key_name+".pem"):
+		if os.path.exists(slf.key_name+".pem"):
 			os.chmod(self.key_name+".pem",0777)
 		f = open(self.key_name+".pem",'w')
 		f.write(self.key_pair.key_material)
 		f.close()
 		os.chmod(f.name,0400)
+	def get_port_number(self, interface_id, router_number):
+		return 60000+interface_id+router_number*100
+	def cloud_shell(self):
+			os.system("xterm -e ssh -i GINI.pem -o StrictHostKeyChecking=no ubuntu@"+self.new_instance_ip+" 'source ~/.profile; sudo -E yRouter/src/yrouter --interactive=1 --verbose=2 --confpath=/home/ubuntu --config=cloud_tunnel Router_1;exec bash'")
+	def local_shell(self):
+			os.system("xterm -e cRouter/src/yrouter --interactive=1 --verbose=2 --confpath=/home/gini/GINI_Cloud --config=local_tunnel Router_1")
 	def create_tunnel(self):
 		# need to copy the yRouter to the cloud
 		print("Creating tunnel")
@@ -86,15 +102,24 @@ class AmazonCloudFunctions:
 		os.system("scp -i "+self.key_name+".pem -o StrictHostKeyChecking=no cloud_tunnel ubuntu@"+self.new_instance_ip+":/home/ubuntu")
 		#start the cloud router
 		# Note you have to delete the files it creates on the cloud after if you want to run it again
-		os.system("xterm -e ssh -i GINI.pem -o StrictHostKeyChecking=no ubuntu@"+self.new_instance_ip+" 'source ~/.profile; sudo -E yRouter/src/yrouter --interactive=1 --verbose=2 --confpath=/home/ubuntu --config=cloud_tunnel Router_1;exec bash'")
+		print("attempting "+ "xterm -e ssh -R "+str(self.get_port_number(0, 1))+":localhost:"+str(self.get_port_number(0,1))+" -i GINI.pem -o StrictHostKeyChecking=no ubuntu@"+self.new_instance_ip+" 'source ~/.profile; sudo -E yRouter/src/yrouter --interactive=1 --verbose=2 --confpath=/home/ubuntu --config=cloud_tunnel Router_1;exec bash'")
+		#os.system("xterm -e ssh -R "+str(self.get_port_number(0, 1))+":localhost:"+str(self.get_port_number(0,1))+"-i GINI.pem -o StrictHostKeyChecking=no ubuntu@"+self.new_instance_ip+" 'source ~/.profile; sudo -E yRouter/src/yrouter --interactive=1 --verbose=2 --confpath=/home/ubuntu --config=cloud_tunnel Router_1;exec bash'")
+		p1 = multiprocessing.Process(target=self.cloud_shell)
+		p1.start()
+		p2 = multiprocessing.Process(target=self.local_shell)
+		print("opening local router")
+		p2.start()
+		
+		#60000+interface_id+router_number*100 
+		# dstport should be the same as the interface id
+		#Name of router is Router_1 where in this case 1 is the router number
 		#start the local router ...
-
 	def create_tunnel_cloud_config_file(self):
 		f = open("cloud_tunnel","w")
 		print("Trying to get public ip")
 		local_ip = urlopen('http://ip.42.pl/raw').read() # Get local public IP
 		print("Got public ip")
-		ifconfig = "ifconfig add tun0 -dstip "+local_ip+" -dstport 1 -addr "+local_ip+" -hwaddr a0:a0:a0:a0:a0\n"
+		ifconfig = "ifconfig add tun0 -dstip "+local_ip+" -dstport 0 -addr 10.10.10.10 -hwaddr a0:a0:a0:a0:a0\n"
 		f.write(ifconfig)
 		route = "route add -dev tun0 -net "+local_ip+" -netmask 255.255.255.255\n"
 		f.write(route)
@@ -105,14 +130,11 @@ class AmazonCloudFunctions:
 			print("Create an instance first!")
 			return
 		f = open("local_tunnel","w")
-		ifconfig = "ifconfig add tun0 -dstip "+self.new_instance_ip+" -dstport 1 -addr "+self.new_instance_ip+" -hwaddr a1:a1:a1:a1:a1\n"
+		ifconfig = "ifconfig add tun0 -dstip "+self.new_instance_ip+" -dstport 0 -addr 20.20.20.20 -hwaddr a1:a1:a1:a1:a1\n"
 		f.write(ifconfig)
 		route = "route add -dev tun0 -net "+self.new_instance_ip+" -netmask 255.255.255.255\n"
 		f.write(route)
 		f.close()
-
-
-
 	def add_udp_rules(self):
 
 		security_group = self.ec2.create_security_group(
