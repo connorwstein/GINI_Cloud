bdb0a4bcadfc2d79a0ae5092cd97d3013c0cb7ea
connorwstein <connor.stein@mail.mcgill.ca>
2016-03-07 19:27:36 -0500

    cleaned up repo

--- client_tunnel_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Allow IP forwarding on your machine /proc/sys/net/ipv4/ip_forward
-echo 1 | tee /proc/sys/net/ipv4/ip_forward;
-# Configure /etc/sshd_config to allow tunneling 
-echo 'PermitTunnel yes' >> /etc/ssh/sshd_config;
-
