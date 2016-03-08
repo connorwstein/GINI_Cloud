721521f4c18e7255e3c8065be0c58f14c627fb9b
connorwstein <connor.stein@mail.mcgill.ca>
2016-03-07 19:10:49 -0500

    delete GINI.pem

---
+++ cRouter/src/udp_client.py
@@ -0,0 +1,7 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto("sup",("52.32.237.58",2464))
+
+
+
+
---
+++ tcp_test.py
@@ -0,0 +1,24 @@
+import socket
+
+TCP_IP = '10.0.1.7'   
+TCP_PORT = 60100
+print("listening on"+str(TCP_PORT))
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+    
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print 'Connection address:', addr
+while 1:
+  data = conn.recv(BUFFER_SIZE)
+  if not data: break
+  print "received data:", data
+  conn.send(data)  # echo
+conn.close()
+
+
+
+
+
